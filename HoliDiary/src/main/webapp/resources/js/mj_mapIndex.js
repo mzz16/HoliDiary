a0913ffef832873d1151b5678d6ec2e758d402ae
minji <busy8863@naver.com>
2022-08-04 00:57:43 +0900

    [mj] 지도 마커찍기

--- HoliDiary/src/main/webapp/resources/js/mj_mapIndex.js
+++ HoliDiary/src/main/webapp/resources/js/mj_mapIndex.js
@@ -1,116 +1,69 @@
-function initAutocomplete() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13,
+let map;
+let markers = [];
+
+const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let labelIndex = 0;
+
+function initMap() {
+    const seoul = { lat: 37.5662952, lng: 126.9779451 };
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: seoul,
         mapTypeId: "roadmap",
     });
     
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, "click", (event) => {
+        addMarker(event.latLng, map);
     });
     
-    let markers = [];
+    // add event listeners for the buttons
+    document
+        .getElementById("show-markers")
+        .addEventListener("click", showMarkers);
+    document
+        .getElementById("hide-markers")
+        .addEventListener("click", hideMarkers);
+    document
+        .getElementById("delete-markers")
+        .addEventListener("click", deleteMarkers);
     
-    // Listen for the event fired when the user selects a prediction and
-	// retrieve
-    // more details for that place.
-    
-    searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
-        if (places.length == 0) {
-            return;
-        }
-        
-        // Clear out the old markers.
-        markers.forEach((marker) => {
-            marker.setMap(null);
-        });
-        
-        markers = [];
-        
-        // For each place, get the icon, name and location.
-        const bounds = new google.maps.LatLngBounds();
-        
-        places.forEach((place) => {
-            if (!place.geometry || !place.geometry.location) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
-            const icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25),
-            };
-            
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-                map,
-                icon,
-                title: place.name,
-                position: place.geometry.location,
-            }));
-            
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            }
-            else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        
-        map.fitBounds(bounds);
-    });
-    
-    const request = {
-            placeId: "ChIJN1t_tDeuEmsRUsoyG83frY4",
-            fields: ["name", "formatted_address", "place_id", "geometry"],
-        };
-        const infowindow = new google.maps.InfoWindow();
-        const service = new google.maps.places.PlacesService(map);
-        service.getDetails(request, (place, status) => {
-            if (status === google.maps.places.PlacesServiceStatus.OK &&
-                place &&
-                place.geometry &&
-                place.geometry.location) {
-                const marker = new google.maps.Marker({
-                    map,
-                    position: place.geometry.location,
-                });
-                google.maps.event.addListener(marker, "click", () => {
-                    const content = document.createElement("div");
-                    const nameElement = document.createElement("h2");
-                    nameElement.textContent = place.name;
-                    content.appendChild(nameElement);
-                    const placeIdElement = document.createElement("p");
-                    placeIdElement.textContent = place.place_id;
-                    content.appendChild(placeIdElement);
-                    const placeAddressElement = document.createElement("p");
-                    placeAddressElement.textContent = place.formatted_address;
-                    content.appendChild(placeAddressElement);
-                    infowindow.setContent(content);
-                    infowindow.open(map, marker);
-                });
-            }
-      });
-    
+    // Add a marker at the center of the map.
+    addMarker(seoul, map);
 }
-window.initAutocomplete = initAutocomplete;
 
+// Adds a marker to the map.
+function addMarker(location, map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    const marker = new google.maps.Marker({
+        position: location,
+        label: labels[labelIndex++ % labels.length],
+        map: map,
+    });
+    markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+// Removes the markers from the map, but keeps them in the array.
+function hideMarkers() {
+    setMapOnAll(null);
+}
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    hideMarkers();
+    markers = [];
+}
+
+
+window.initMap = initMap;
 export {};
-
-
-
-
-
-
-
-
-
