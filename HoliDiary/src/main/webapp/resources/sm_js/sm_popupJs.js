11f6214e5fd4840647f953fd7b24d76fe784e354
KIMSUMIN <kmanvy@naver.com>
2022-08-04 10:41:50 +0900

    [sm] 커밋

--- HoliDiary/src/main/webapp/resources/sm_js/sm_popupJs.js
+++ HoliDiary/src/main/webapp/resources/sm_js/sm_popupJs.js
@@ -1,5 +1,5 @@
 function popup() {
 	let options = "toolbar=no,scrollbars=no,resizable=yes,status=no,menubar=no,width=1000, height=600, top=100, left=270";
 
-	window.open("popup.jsp","_blank", options);
+	window.open("popup.open","_blank", options);
 }
