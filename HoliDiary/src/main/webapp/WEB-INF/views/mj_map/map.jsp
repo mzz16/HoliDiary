6ebcdb52cbc7899e10293976673abacddf0ca1e2
minji <busy8863@naver.com>
2022-08-05 17:46:14 +0900

    [mj] 지도 retriction, 대륙버튼 눌렀을 시 이동, 좌표값 불러오기 기능 구현

--- HoliDiary/src/main/webapp/WEB-INF/views/mj_map/map.jsp
+++ HoliDiary/src/main/webapp/WEB-INF/views/mj_map/map.jsp
@@ -1,3 +1,5 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" language="java" %>
+
 <html>
 <head>
 <title>Place Autocomplete and Directions</title>
@@ -10,19 +12,25 @@
 <body>
 
 	<input id="pac-input" class="controls" type="text" placeholder="Search Box" />
+	
+	<button id="europeGo">유럽</button>
+	<button id="asiaGo">아시아</button>
+	<button id="americaGo">아메리카</button>
+	<button id="oceaniaGo">오세아니아</button>
+	<button id="africaGo">아프리카</button>
 
-	<div id="floating-panel">
+	<!-- <div id="floating-panel">
 		<input id="hide-markers" type="button" value="Hide Markers" /> 
 		<input id="show-markers" type="button" value="Show Markers" /> 
 		<input id="delete-markers" type="button" value="Delete Markers" />
-	</div>
+	</div> -->
 
 	<div id="map"></div>
 	
 	<p>
 
 	<script
-		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb4vTBtJ5eI4DOAJXj4ov7YSdM066-PQ0&libraries=places&callback=initMap&v=weekly"
+		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDb4vTBtJ5eI4DOAJXj4ov7YSdM066-PQ0&callback=initMap&v=weekly&libraries=places"
 		defer></script>
 </body>
 </html>
--- HoliDiary/src/main/webapp/resources/mj_js/mj_mapIndex.js
+++ HoliDiary/src/main/webapp/resources/mj_js/mj_mapIndex.js
@@ -1,19 +1,62 @@
+let latLng = { lat: 37.5662952, lng: 126.9779451 };
+let europe = { lat: 56.0789635, lng: 0.9901297 };
+let asia = { lat: 34.047863, lng: 100.6196553 };
+let america = { lat: 11.863562, lng: -107.4353043 };
+let oceania = { lat: -11.1692535, lng: 130.6697515 };
+let africa = { lat: 4.364132, lng: 23.025819 };
+const bounds = {
+		north: 84.860203,
+		south: -84.618493,
+		west: -180.0,
+		east: 180.0
+};
+
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let labelIndex = 0;
 
+	
 function initMap() {
-	  const seoul = { lat: 37.5662952, lng: 126.9779451 };
-	  const map = new google.maps.Map(document.getElementById("map"), {
+	
+	  let map = new google.maps.Map(document.getElementById("map"), {
 	    zoom: 13,
-	    center: seoul,
+	    center: latLng,
+	    restriction: {
+            latLngBounds: bounds,
+            strictBounds: false,
+        },
 	  });
+	  
+	  
+	  $('#europeGo').on('click', function() {
+			map.panTo(europe);
+			map.setZoom(4);
+		});
+	  $('#asiaGo').on('click', function() {
+		  map.panTo(asia);
+		  map.setZoom(3);
+	  });
+	  $('#americaGo').on('click', function() {
+		  map.panTo(america);
+		  map.setZoom(3);
+	  });
+	  $('#oceaniaGo').on('click', function() {
+		  map.panTo(oceania);
+		  map.setZoom(4);
+	  });
+	  $('#africaGo').on('click', function() {
+		  map.panTo(africa);
+		  map.setZoom(3);
+	  });
+		
+	  
+	  
 	  const contentString = '<input id="a" placeholder="장소">'
 	   
 	  let infowindow = new google.maps.InfoWindow({
 	    content: contentString,
 	  });
 	  let marker = new google.maps.Marker({
-	    position: seoul,
+	    position: latLng,
 	    map,
 	    title: "zz",
 	  });
@@ -32,30 +75,37 @@ function initMap() {
 	  // This event listener calls addMarker() when the map is clicked.
 	  google.maps.event.addListener(map, "click", (event) => {
 		  console.log('마커찍힘')
+		  console.log(event.latLng.lat())
+		  console.log(event.latLng.lng())
 		  addMarker(event.latLng, map);
     
 	  });
 
 	  // add event listeners for the buttons
-	  document.getElementById("show-markers").addEventListener("click", showMarkers);
-	  document.getElementById("hide-markers").addEventListener("click", hideMarkers);
-	  document.getElementById("delete-markers").addEventListener("click", deleteMarkers);
+// document.getElementById("show-markers").addEventListener("click",
+// showMarkers);
+// document.getElementById("hide-markers").addEventListener("click",
+// hideMarkers);
+// document.getElementById("delete-markers").addEventListener("click",
+// deleteMarkers);
 
 	  // Add a marker at the center of the map.
-	  //addMarker(seoul, map);
+	  // addMarker(seoul, map);
 
 }
 
 
 
 
-//Adds a marker to the map.
+// Adds a marker to the map.
 function addMarker(location, map) {
 	  let newMaker = new google.maps.Marker({
 		    position: location,
 		    label: labels[labelIndex++ % labels.length],
 		    map: map,
 	  });
+	 
+	  
 	  newMaker.addListener("click", () => {
 	  		console.log('추가된 마커 이벤트')
 	  		// 찍힌 마커에 해당 정보 가져오기
@@ -66,6 +116,8 @@ function addMarker(location, map) {
 				let infowindow = new google.maps.InfoWindow({
 		  			content: infos,
 		  		});
+				
+				
 		  		infowindow.open({
 		  			anchor: newMaker, map,
 		  			shouldFocus: false,
@@ -96,25 +148,13 @@ function addMarker(location, map) {
 window.initMap = initMap;
 
 
-
-//Sets the map on all markers in the array.
-function setMapOnAll(map) {
-for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-}
-}
-//Removes the markers from the map, but keeps them in the array.
-function hideMarkers() {
-setMapOnAll(null);
-}
-//Shows any markers currently in the array.
-function showMarkers() {
-setMapOnAll(map);
-}
-//Deletes all markers in the array by removing references to them.
-function deleteMarkers() {
-hideMarkers();
-markers = [];
-}
-
+/*
+ * // Sets the map on all markers in the array. function setMapOnAll(map) { for
+ * (let i = 0; i < markers.length; i++) { markers[i].setMap(map); } } // Removes
+ * the markers from the map, but keeps them in the array. function hideMarkers() {
+ * setMapOnAll(null); } // Shows any markers currently in the array. function
+ * showMarkers() { setMapOnAll(map); } // Deletes all markers in the array by
+ * removing references to them. function deleteMarkers() { hideMarkers();
+ * markers = []; }
+ */
 
