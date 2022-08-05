6da8f815d9e57500b827f50b17c0aea6dd7e956c
sej1004 <107029371+sej1004@users.noreply.github.com>
2022-08-05 17:47:00 +0900

    Sej (#31)
    * [sej] Create second slider + Update menu bar
    
    * [sej] 하단 슬라이드 작업 및 로고 홈 연결 + 메뉴바 수정

--- HoliDiary/src/main/webapp/resources/sej_js/sej_home.js
+++ HoliDiary/src/main/webapp/resources/sej_js/sej_home.js
@@ -28,11 +28,74 @@ function mainSliderEvent() {
 	    	  slidesToScroll: 1,
 	    	  autoplay: true,
 	    	  autoplaySpeed: 2000,
-	    	  arrows : true,         // 옆으로 이동하는 화살표 표시 여부
+	    	  arrows : true,      // 옆으로 이동하는 화살표 표시 여부
 	          dots : true, 
 	      });
 }
 
+/*function secondSliderEvent() {
+	$('.second_slider').slick({
+		  dots: true,
+		  arrows : true,
+		  infinite: false,
+		  speed: 300,
+		  slidesToShow: 4,
+		  slidesToScroll: 4,
+		  responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		    // You can unslick at a given breakpoint now by adding:
+		    // settings: "unslick"
+		    // instead of a settings object
+		  ]
+		});
+}
+*/
+document.addEventListener("DOMContentLoaded", function() {
+
+    var mySwiper = new Swiper('.swiper1', {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        observer: true,
+        observeParents: true,
+        spaceBetween: 24,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1280: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+            },
+            720: {
+                slidesPerView: 1,
+                slidesPerGroup: 1,
+            }
+        }
+    });    
+});
 
 
 
@@ -42,4 +105,6 @@ $(function() {
 	closeNav();*/
 	/*closeNav2();*/
 	mainSliderEvent();
+	secondSliderEvent();
+
 });
