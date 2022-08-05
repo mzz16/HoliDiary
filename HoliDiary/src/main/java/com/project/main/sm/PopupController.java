1d5b818aaea9fe47f08ebdc9c9ac0b108a46902f
KimSumin0 <107032817+KimSumin0@users.noreply.github.com>
2022-08-05 17:48:50 +0900

    [sm] 팝업창 설정 틀, 세션 스코프 (#34)
    * [sm] commit for pull
    
    * [sm] 팝업창 설정 관리 틀, 세션 스코프

--- HoliDiary/src/main/java/com/project/main/sm/PopupController.java
+++ HoliDiary/src/main/java/com/project/main/sm/PopupController.java
@@ -2,25 +2,38 @@ package com.project.main.sm;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.project.main.js.UserDAO;
+
 @Controller
 public class PopupController {
+
+	@Autowired
+	private UserDAO uDAO;
 	
+	//팝업창 - 오픈
 	@RequestMapping(value = "/popup.open", method = RequestMethod.GET)
 	public String popupOpen(HttpServletRequest req) {
+		if (uDAO.loginCheck(req)) {
+			req.setAttribute("popupContentPage", "popupHome.jsp");
+			return "ksm_main/popup";			
+		} else {
+			req.setAttribute("popupContentPage", "popupBack.jsp");
+			return "ksm_main/popupBack";			
+		}
 		
-		req.setAttribute("contentPage", "popupHome.jsp");
-		return "ksm_main/popup";
 	}
 	
 	//팝업창 - 홈
 	@RequestMapping(value = "/popupHomeGo", method = RequestMethod.GET)
 	public String popupHome(HttpServletRequest req) {
 		
-		req.setAttribute("contentPage", "popupHome.jsp");
+		uDAO.loginCheck(req);
+		req.setAttribute("popupContentPage", "popupHome.jsp");
 		return "ksm_main/popup";
 	}
 	
@@ -28,8 +41,19 @@ public class PopupController {
 	@RequestMapping(value = "/popupScheduleGo", method = RequestMethod.GET)
 	public String popupScedule(HttpServletRequest req) {
 		
-		req.setAttribute("contentPage", "popupSchedule.jsp");
+		uDAO.loginCheck(req);
+		req.setAttribute("popupContentPage", "popupSchedule.jsp");
 		return "ksm_main/popup";
 	}
+	
+	//팝업창 - 설정 관리
+	@RequestMapping(value = "/updateMyPopup", method = RequestMethod.GET)
+	public String updateMyPopup(HttpServletRequest req) {
+		
+		uDAO.loginCheck(req);
+		req.setAttribute("popupContentPage", "updateMyPopup.jsp");
+		return "ksm_main/popup";
+	}
+	
 
 }
