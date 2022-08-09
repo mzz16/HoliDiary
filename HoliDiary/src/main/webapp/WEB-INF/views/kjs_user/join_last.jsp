eae0291fb36505b78dfb7a434ad4f5c2ccb8dc9c
Jisu Kim <tlawl912@gmail.com>
2022-08-09 17:48:48 +0900

    [JS] 회원가입 유효성 검사 추가

--- HoliDiary/src/main/webapp/WEB-INF/views/kjs_user/join_last.jsp
+++ HoliDiary/src/main/webapp/WEB-INF/views/kjs_user/join_last.jsp
@@ -9,11 +9,45 @@
 <body>
 
 <div id="content_join">
-	
-			HOlIDAIRY에 오신걸 환영합니다
-			 ${r}
-	
-	</div>
+	<div class="join_file_upload_box">
 
+	<div class="join_phrase">
+		<span>HOlIDAIRY에 오신걸 환영합니다.</span>
+		<span>마지막 프로필 사진을 등록해주세요.</span>
+	</div>
+		<div class="join_go_home_box">
+			<button onclick="goHome()">나중에 등록하기</button>
+		</div>
+		${r}
+		
+		
+		<div id="join_img_box">
+			<img id="join_preview_img">
+		</div>
+			<form action="join.file.upload" method="post" enctype="multipart/form-data">
+					<div class="join_file_input_box">
+						<input type="file" onchange="readURL(this)" name="userImg">
+					</div>
+					<div class="join_file_btn_box">
+						<button>등록하기</button>
+					</div>
+			</form>
+	</div>
+	</div>
+	
+	<script type="text/javascript">
+	function readURL(input) {
+		  if (input.files && input.files[0]) {
+		    var reader = new FileReader();
+		    reader.onload = function(e) {
+		      document.getElementById('join_preview_img').src = e.target.result;
+		    };
+		    reader.readAsDataURL(input.files[0]);
+		  } else {
+		    document.getElementById('join_preview_img').src = "";
+		  }
+		}
+	</script>
 </body>
+
 </html>
--- HoliDiary/src/main/webapp/resources/kjs_js/kjs_valid.js
+++ HoliDiary/src/main/webapp/resources/kjs_js/kjs_valid.js
@@ -1,6 +1,58 @@
 // <input>을 넣었을때
 // 비어있으면 true, 글자가 들어있으면 false
 function isEmpty(input) {
+	return (!input.val());
+}
+
+function isEmptyForJS(input) {
 	return (!input.value);
 }
 
+//글자수가 모자라면 true, 괜찮으면 false
+function lessThan(input, len) {
+	return (input.val().length < len);
+}
+
+function lessThanForJS(input, len) {
+	return (input.value.length < len);
+}
+
+//영어, 숫자이외의 것이 있으면 true, 아니면 false
+function containsHS(input) {
+	var txt = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@_.";
+	for (var i = 0; i < input.val().length; i++) {
+		if (txt.indexOf(input.val()[i]) == -1) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function containsHSForJS(input) {
+	var txt = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM@_.";
+	for (var i = 0; i < input.value.length; i++) {
+		if (txt.indexOf(input.value[i]) == -1) {
+			return true;
+		}
+	}
+	return false;
+}
+
+//두 개 내용이 다르면 true, 같으면 false
+function notEquals(input1, input2) {
+	return (input1.val() != input2.val());
+}
+
+function notEqualsForJS(input1, input2) {
+	return (input1.value != input2.value);
+}
+
+//문자열세트에 포함된게 없으면 true, 있으면 false
+function notContains(input, set) {
+	for (var i = 0; i < set.length; i++) {
+		if (input.value.indexOf(set[i]) != -1) {
+			return false;
+		}
+	}
+	return true;
+}
