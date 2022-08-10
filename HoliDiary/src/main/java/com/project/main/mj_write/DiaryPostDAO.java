d4fb71b2ffc4eb07a9f2f2fd682e2ac8db704459
minji <busy8863@naver.com>
2022-08-10 19:23:55 +0900

    [mj] postDAO 이름 수정

--- HoliDiary/src/main/java/com/project/main/mj_write/DiaryPostDAO.java
+++ HoliDiary/src/main/java/com/project/main/mj_write/DiaryPostDAO.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PostDAO {
+public class DiaryPostDAO {
 
 	@Autowired
 	private SqlSession ss;
